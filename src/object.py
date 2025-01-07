291a74ed48d3d65c4eb38e7403907b20e14fbfd1
Вилков Виталий <takao.nakamuro@yandex.ru>
2025-01-07 22:45:47 +0300

    Сделал суммарное покрытие тестами 77%

--- src/object.py
+++ src/object.py
@@ -34,11 +34,12 @@ class Product(BaseProduct, CreationLogMixin):
     def __init__(self, name: str, description: str, price: float, quantity: int):
         if quantity <= 0:
             raise ValueError("Товар с нулевым количеством не может быть добавлен.")
-        super().__init__(name, description, price, quantity)
-        self.__price = price  # Цена становится приватной
-        self.quantity = quantity
-        self.name = name
-        self.description = description
+        else:
+            super().__init__(name, description, price, quantity)
+            self.__price = price  # Цена становится приватной
+            self.quantity = quantity
+            self.name = name
+            self.description = description
 
     def __repr__(self):
         return (f"(name='{self.name}', "
@@ -69,18 +70,16 @@ class Product(BaseProduct, CreationLogMixin):
     @price.setter
     def price(self, value: float):
         if value <= 0:
-            print("Цена не должна быть нулевая или отрицательная")
-        else:
-            self.__price = value
+            raise ValueError("Цена не должна быть нулевая или отрицательная")  # Изменено на выброс исключения
+        self.__price = value
 
     def get_info(self):
         return f"Продукт: {self.name}, цена: {self.__price}, количество: {self.quantity}"
 
     def add_stock(self, amount: int):
-        if amount > 0:
-            self.quantity += amount
-        else:
-            print("Количество добавляемого товара должно быть положительным.")
+        if amount <= 0:
+            raise ValueError("Количество добавляемого товара должно быть положительным.")
+        self.quantity += amount
 
 
 # Класс Smartphone, наследующий от Product
@@ -157,9 +156,7 @@ class Category:
 
     def middle_price(self):
         """Метод для подсчета средней цены товаров в категории."""
-        try:
-            total_price = sum(product.price for product in self._products)
-            average = total_price / len(self._products)
-        except ZeroDivisionError:
+        if not self._products:  # Проверка на пустой список
             return 0
-        return average
+        total_price = sum(product.price for product in self._products)
+        return total_price / len(self._products)
